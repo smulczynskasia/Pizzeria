154021225528ee4a564769d7b137fcff74010ead
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-07-29 09:09:51 +0200

    Add active images

--- src/js/script.js
+++ src/js/script.js
@@ -88,6 +88,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     
     initAccordion(){
@@ -156,15 +157,27 @@
           console.log(optionId, option);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(optionSelected) {
             // check if the option is not default 
             if(!option.default == true) {
               // add option price to price variable
               price += option.price;
+
             }
           } else if(option.default == true) {
             // reduce price variable
             price -= option.price;
+          }
+
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          if(optionImage) {
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+              console.log('dodany obrazek:',classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
           }
         }
       }
