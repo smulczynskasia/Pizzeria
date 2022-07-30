ef8973ead704f21d0027ee8b8dfc41be50c39e62
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-07-30 19:28:54 +0200

    Add class Cart && cart toggler

--- src/js/script.js
+++ src/js/script.js
@@ -221,6 +221,33 @@
       });
     }
   }
+
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions(element);
+      console.log('new Cart:', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      
+    }
+
+    initActions(){
+      const thisCart = this;
+      
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -234,7 +261,14 @@
     initData: function(){
       const thisApp = this;
       thisApp.data = dataSource;
-    },  
+    },
+	
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
 
     init: function(){
       const thisApp = this;
@@ -246,6 +280,7 @@
     
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
   class AmountWidget{
