13d807c1f572a9dd4d015b36069b5fce42162102
smulczynskasia <smulczynska.katarzyna@gmail.com>
2023-01-01 19:37:56 +0000

    Add server.mjs

--- server.mjs
+++ server.mjs
@@ -1,8 +1,8 @@
-/* global require, process */
+import path from 'path';
+import jsonServer from 'json-server';
 
-const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('dist/db/app.json');
+const router = jsonServer.router(path.join('dist', 'db', 'app.json');
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
@@ -13,3 +13,5 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+
+export default server;
